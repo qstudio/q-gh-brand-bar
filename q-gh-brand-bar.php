855960edff1af69eade298fea5cb8999f0f3d354
Roman K <zarok1987@gmail.com>
2016-12-29 19:35:39 +0200

    Version # updated

--- q-gh-brand-bar.php
+++ q-gh-brand-bar.php
@@ -13,7 +13,7 @@
  * Plugin Name:     Global Branding Bar
  * Plugin URI:      http://qstudio.us/
  * Description:     Global branding bar for all Greenheart Sites
- * Version:         0.4.9
+ * Version:         0.4.10
  * Author:          Q Studio
  * Author URI:      http://qstudio.us
  * License:         GPL2
