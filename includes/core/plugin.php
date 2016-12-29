12370e4e2cbb60108404613885ec5639ae2dddee
Roman K <zarok1987@gmail.com>
2016-12-29 20:32:38 +0200

    GHT mobile fix - 3

--- includes/core/plugin.php
+++ includes/core/plugin.php
@@ -12,7 +12,7 @@ use Q_GH_Brand_Bar\Theme\Template as Template;
 class Plugin {
 
 	// Settings ##
-    protected $version = '0.4.13';
+    protected $version = '0.4.14';
     static $device; // current device handle ( 'desktop || handheld' )##
     protected static $debug = true;
 
