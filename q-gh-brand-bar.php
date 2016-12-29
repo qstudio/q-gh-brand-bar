bd9c2533a36344a3f82e244c8d82c7bd425aaad6
Roman K <zarok1987@gmail.com>
2016-12-29 20:19:35 +0200

    GHT mobile fix - 2

--- q-gh-brand-bar.php
+++ q-gh-brand-bar.php
@@ -13,7 +13,7 @@
  * Plugin Name:     Global Branding Bar
  * Plugin URI:      http://qstudio.us/
  * Description:     Global branding bar for all Greenheart Sites
- * Version:         0.4.12
+ * Version:         0.4.13
  * Author:          Q Studio
  * Author URI:      http://qstudio.us
  * License:         GPL2
--- readme.txt
+++ readme.txt
@@ -3,7 +3,7 @@ Contributors: qlstudio
 Tags: custom plugin
 Requires at least: 4
 Tested up to: 4.7
-Stable tag: 0.4.12
+Stable tag: 0.4.13
 License: GPL
 
 Add a global branding bar to all templates 
