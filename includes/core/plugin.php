96d11d132641c1fb0d23e47450670acbd6b07dba
Viktor <v.bredikhin@myfeelix.de>
2019-01-23 13:17:21 +0300

    style fix

--- includes/core/plugin.php
+++ includes/core/plugin.php
@@ -12,7 +12,7 @@ use Q_GH_Brand_Bar\Theme\Template as Template;
 class Plugin {
 
 	// Settings ##
-    static $version = '0.7.4';
+    static $version = '0.7.5';
     static $device; // current device handle ( 'desktop || handheld' ) ##
     protected static $debug = true;
     static $name = 'q-gh-bb';
